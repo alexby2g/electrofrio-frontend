ed5a8a1e790ff522864adbcf384e8a09fa3446b3
Alexander <alexby2g@gmail.com>
2026-05-22 21:57:07 -0400

    backend render url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://127.0.0.1:8000/api'
+  baseURL: 'https://electrofrio-backend.onrender.com/api'
 })
 
 export default boot(({ app }) => {
