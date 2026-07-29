7b4ed67ddfc78bf915bd9390caa44b9e504b7de7
alexby2g <alexby2g@gmail.com>
2026-07-29 17:12:17 -0400

    feat: registrar ruta de WhatsApp Business

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,7 @@ const routes = [
       { path: '', redirect: '/dashboard' },
       { path: 'dashboard', name: 'dashboard', component: () => import('pages/DashboardPage.vue') },
       { path: 'clientes', name: 'clientes', component: () => import('pages/ClientesPage.vue') },
+      { path: 'whatsapp', name: 'whatsapp', component: () => import('pages/WhatsAppPage.vue') },
       { path: 'tecnicos', name: 'tecnicos', component: () => import('pages/TecnicosPage.vue') },
       { path: 'equipos', name: 'equipos', component: () => import('pages/EquiposPage.vue') },
       { path: 'servicios', name: 'servicios', component: () => import('pages/ServiciosPage.vue') },
